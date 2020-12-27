323ba463658e6808f36a5926c9cb8908c9912ada
Colin <skow0020@gmail.com>
2020-12-27 15:13:31 -0600

    Format fixes

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
 import json
 import os
 
-import psycopg2
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-# DATABASE_URL = os.environ['DATABASE_URL']
-
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-# conn = psycopg2.connect(DATABASE_URL)
-
-# if conn is not None:
-#     print('Connection established to PostgreSQL.')
-# else:
-#     print('Connection not established to PostgreSQL.')
 
 app = Flask(__name__)
 
@@ -22,6 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import *
+
 
 @app.route('/')
 def hello_world():
@@ -34,17 +25,6 @@ def get_books():
     return {
         'book': books[0].title
     }
-    # with conn:
-    #     with conn.cursor() as curs:
-    #         curs.execute("SELECT * FROM books;")
-
-    #         records = curs.fetchall()
-    #         print("Total rows are:  ", len(records))
-    #         print("Printing each row")
-    #         for row in records:
-    #             print("Id: ", row[0])
-
-    #         return json.dumps(records)
 
 
 if __name__ == '__main__':
